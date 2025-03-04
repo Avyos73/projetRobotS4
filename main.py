3b3c63d23fcf9ec8029b7f013fbc8bcf8149e06a
Avyos73 <vikthor.aubry@gmail.com>
2025-03-04 14:57:01 +0100

    moui

--- main.py
+++ main.py
@@ -23,7 +23,9 @@ def manual():
             if event.type == pygame.JOYAXISMOTION:
                 x_axis = round(joystick.get_axis(0),1)  # Axe X du stick gauche
                 y_axis = round(joystick.get_axis(1),1)
-                
+            
+            if event.type==pygame.JOYBUTTONDOWN:
+                return
         
         # Vérifier périodiquement les valeurs des axes même sans événement
         x_axis = round(joystick.get_axis(0),1)  # Axe X du stick gauche
@@ -41,7 +43,7 @@ def auto():
     sleep(angle*temps2pi/360)
     rbt.goto(0,0,0)
 
-    while True:
+    while rbt.score < 10:
         obj = rbt.getObjectif()
         rbt.goto(obj[0],obj[1],0)  
 
